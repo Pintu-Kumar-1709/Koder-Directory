e015027d9546c165c969efd4ebdaa9fbcea6012c
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-04-18 08:40:35 +0530

    added 1st repo

---
+++ 01_react/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h4 className="bg-blue-400 text-black text-3xl rounded-3xl p-2 uppercase">
+        tailwindcss css
+      </h4>
+      <Card />
+    </>
+  );
+}
+
+export default App;
