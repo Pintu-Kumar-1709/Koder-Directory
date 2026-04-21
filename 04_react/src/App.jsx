d631b1df3d96cc46d2294e4aa25e16f3934c9176
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-04-21 09:00:10 +0530

    added main file

---
+++ 04_react/src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from "./Counter.jsx";
+import "./App.css";
+import LikeButton from "./LikeButton.jsx";
+function App() {
+  return (
+    <>
+      <h4>States in React</h4>
+      <Counter />
+      <LikeButton />
+    </>
+  );
+}
+
+export default App;
