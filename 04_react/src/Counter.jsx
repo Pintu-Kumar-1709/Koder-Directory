d4f14a3a822f35c1826e03b65daffc1e487aa0e4
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-04-21 08:57:00 +0530

    added counter fn

---
+++ 04_react/src/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Counter() {
+  let [Count, setCount] = useState(0);
+
+  let incCount = () => {
+    setCount(Count + 1);
+    console.log(Count);
+  };
+
+  return (
+    <div>
+      <h3 id="h3">Count = {Count}</h3>
+      <button onClick={incCount}>Increase Count</button>
+    </div>
+  );
+}
+
+export default Counter;
