024cd235163d062dd968e07a48593e987496a2d4
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-04-28 16:51:33 +0530

    implemented services page

---
+++ react-router-dom/src/pages/Services.jsx
@@ -0,0 +1,10 @@
+const Services = () => {
+  return (
+    <div>
+      <h1>Services page</h1>
+      <h2>[Web developement, Software developement]</h2>
+    </div>
+  );
+};
+
+export default Services;
