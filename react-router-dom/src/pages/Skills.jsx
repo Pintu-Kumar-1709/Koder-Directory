d5ba92b9dd5847a3620fda6f206ab144404c494b
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-04-28 16:52:02 +0530

    implemented skills page

---
+++ react-router-dom/src/pages/Skills.jsx
@@ -0,0 +1,12 @@
+
+
+const Skills = () => {
+  return (
+    <div>
+        <h1>Skills Page</h1>
+        <h2>[React.js, Next.js, Tailwind-CSS, ShadcnUi]</h2>
+    </div>
+  )
+}
+
+export default Skills
